472d9ca5af7fa704c346a75f8c8e743d2baa8c43
Sunil Dagur <sunildagur566@gmail.com>
2023-12-05 18:42:44 +0530

    Add project files.

---
+++ Assignment 5_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+           
+            int value = numbers[10];
+
+            
+            Console.WriteLine("Value at index 10: " + value);
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.ReadLine(); 
+    }
+}
